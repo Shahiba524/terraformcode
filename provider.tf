ddeb0c9736c89f197230b422ced560351f4e00fc
Shahiba524 <109478690+Shahiba524@users.noreply.github.com>
2022-09-16 00:23:17 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,11 +1,12 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "4.29.0"
+      source = "hashicorp/aws"
+      version = "4.30.0"
     }
   }
 }
+
 provider "aws" {
   # Configuration options
 }
